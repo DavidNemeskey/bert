da2bf4d306aad749feb1947ddce311438cfcc678
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-11-27 16:01:58 +0100

    Stuff needs to be run on each vm instance.

---
+++ vm_init.sh
@@ -0,0 +1,9 @@
+gcloud config set compute/region europe-west4
+gcloud config set compute/zone europe-west4-a
+sudo pip3 install gast==0.2.2
+# Not adding this as this should be done on a tmux basis
+# export TPU_NAME=tpu-test
+# BERT
+git clone https://github.com/DavidNemeskey/bert.git
+# ALBERT in the albert directory
+git clone https://github.com/DavidNemeskey/google-research.git
